e2a9203fe946c6032be18ddbd4961fe9388335fd
sainiaman20 <sainiaman3923@gmail.com>
2024-10-05 01:32:49 +0530

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+console.log("I'm ready!");
 // Iteration 1: Names and Input
-//
+let hacker1 = "Aman";
+console.log(`The driver's name is ${hacker1}.`);
+
+let hacker2 = "Rio";
+console.log(`The navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
 
 
